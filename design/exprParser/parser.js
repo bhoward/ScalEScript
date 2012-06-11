0ac973075439de59684a07ac929a89029cdc0655
Brian Howard <bhoward@depauw.edu>
2012-06-11 16:15:01 -0400

    Added Scala source to parser.js

--- design/exprParser/parser.js
+++ design/exprParser/parser.js
@@ -1,6 +1,27 @@
 // By-Hand Translation of Scala Expression Parser/Evaluator
 // Brian Howard, Summer 2012
 
+// Original Scala source (not including the Option/Some/None part):
+/*
+sealed trait Expression {
+}
+
+case class Constant(value: Double) extends Expression
+case class Sum(left: Expression, right: Expression) extends Expression
+case class Difference(left: Expression, right: Expression) extends Expression
+case class Product(left: Expression, right: Expression) extends Expression
+case class Quotient(left: Expression, right: Expression) extends Expression
+
+object Expression {
+  def eval(e: Expression): Double = e match {
+    case Constant(value) => value
+    case Sum(left, right) => eval(left) + eval(right)
+    case Difference(left, right) => eval(left) - eval(right)
+    case Product(left, right) => eval(left) * eval(right)
+    case Quotient(left, right) => eval(left) / eval(right)
+  }
+}
+*/
 
 var Option = function Option(x) {
 	if (this instanceof Option) {
