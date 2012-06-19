99d9ddca16c6ab8978bd88ac57f5cd7ae9bc44d6
Trevor Griswold <tgriswol@willamette.edu>
2012-06-19 14:03:10 -0400

    Tweaked the html to include a css file, not a style tag

--- CompilerV0/src/compilerV0/Main.scala
+++ CompilerV0/src/compilerV0/Main.scala
@@ -68,63 +68,19 @@ object Main {
     println("Parsed Expression: " + Parser("""println("hello world")"""))
     println("Code Generated: " + CodeGeneration(Parser("""println("hello world")""")))
     
-    val scalaSource = """{println("Here we go!"); println( if (5 > 3) 5 else 6 ) }""";
+    println();
+    
+    val scalaSource = """{println("Here we go!"); println( 1 + 3 * 5 ) }""";
     val jsSource = CodeGeneration(Parser(scalaSource));
     
     println(makeHTML(scalaSource, jsSource));
   }
   
   def makeHTML(scalaSource : String, jsSource : String) : String = {
-    val p1 : String = 
-      """<html lang="en">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<title>CompilerV0</title>
-    <style type="text/css" media="screen">
-    body {
-        white-space-collapse: discard;
-        float: left;
-        background: silver;
-    }
-    div.ScalaCode {
-        background: white;
-        color: black;
-        font-family: Courier New;
-        white-space: pre;
-    }
-    div.JSCode {
-        background: white;
-        color: black;
-        font-family: Courier New;
-        white-space: pre;
-    }
-    div.JSExec {
-        background: white;
-        color: black;
-        font-family: Courier New;
-        white-space: pre;
-    }
-    </style>
-</head>
-
-<body>
-<h1>Scala to Javascript compiler test page </h1>
-
-<h2>Scala Source:</h2>
-<div class="ScalaCode">""";
-    val p2 : String = """
-</div>
-
-<h2>Javascript Source:</h2>
-<div class="JSCode">""";
-    val p3 : String = """
-</div>
-
-<h2>Javascript Execution:</h2>
-<div class="JSExec"><script language="JavaScript">""";
-    val p4 : String = """</script></div>
-
-</body>""";
+    val p1 : String = """<html lang="en"><head><meta http-equiv="content-type" content="text/html; charset=utf-8"><title>CompilerV0</title><link rel="stylesheet" type="text/css" href="CompilerV0.css" /></head><body><h1>Scala to Javascript compiler test page </h1><h2>Scala Source:</h2><div class="ScalaCode">""";
+    val p2 : String = """</div><h2>Javascript Source:</h2><div class="JSCode">""";
+    val p3 : String = """</div><h2>Javascript Execution:</h2><div class="JSExec"><script language="JavaScript">""";
+    val p4 : String = """</script></div></body>""";
     
     return p1 + scalaSource + p2 + jsSource + p3 + jsSource + p4;
   }
