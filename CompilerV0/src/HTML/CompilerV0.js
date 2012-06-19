4678a3f098fd5c159e3111b6a070ee56ca991a9f
Trevor Griswold <tgriswol@willamette.edu>
2012-06-19 15:27:57 -0400

    Created a Template, JavaScript library, and stylesheets for testing JS

---
+++ CompilerV0/src/HTML/CompilerV0.js
@@ -0,0 +1,24 @@
+function ifThen(test, trueClause) {
+    if (test) {
+        return trueClause;
+    }
+}
+function ifThenElse(test, trueClause, falseClause) {
+    if (test) {
+        return trueClause;
+    } else {
+        return falseClause;
+    }
+}
+function whileLoop(test, body) {
+    while (test) {
+        body;
+    }
+}
+
+function Nint(num) {
+    return num;
+}
+function Ndouble(num) {
+    return num;
+}
