fd18527e91f0683a5c4c84883957d2512abc1f7d
Trevor Griswold <tgriswol@willamette.edu>
2012-06-19 13:50:55 -0400

    Added helper function for building HTML code

--- CompilerV0/src/compilerV0/Main.scala
+++ CompilerV0/src/compilerV0/Main.scala
@@ -23,13 +23,11 @@ object Main {
     println(Parser("if (5 >= 6) 5 else 6"))
     println(Parser("if (5 <= 5) 5 else 6"))
     println(Parser("if (5 < 5) 5 else 6"))
-    */
+    
     //While statement
     println(Parser("while (true) 5"))
     
-    
     //Block statement
-    /*
     println(Parser("{ 1; 2; 3; 4; }"))
     println(Parser("{ 1; 2; 3; 4 }"))
     println(Parser("{ 1; }"))
@@ -41,6 +39,7 @@ object Main {
     //Print and println statement
     println(Parser("print(5)"));
     println(Parser("""println("hello world")"""))
+    
     println();
     /* CodeGeneration testing */
     
@@ -62,5 +61,64 @@ object Main {
     println("Parsed Expression: " + Parser("{ 1; 2; 3; 4; }"))
     println("Code Generated: " + CodeGeneration(Parser("{ 1; 2; 3; 4; }")))
     
+    val scalaSource = """{println("Here we go!"); println( if (5 > 3) 5 else 6 ) }""";
+    val jsSource = CodeGeneration(Parser(scalaSource));
+    
+    println(makeHTML(scalaSource, jsSource));
+  }
+  
+  def makeHTML(scalaSource : String, jsSource : String) : String = {
+    val p1 : String = 
+      """<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>CompilerV0</title>
+    <style type="text/css" media="screen">
+    body {
+        white-space-collapse: discard;
+        float: left;
+        background: silver;
+    }
+    div.ScalaCode {
+        background: white;
+        color: black;
+        font-family: Courier New;
+        white-space: pre;
+    }
+    div.JSCode {
+        background: white;
+        color: black;
+        font-family: Courier New;
+        white-space: pre;
+    }
+    div.JSExec {
+        background: white;
+        color: black;
+        font-family: Courier New;
+        white-space: pre;
+    }
+    </style>
+</head>
+
+<body>
+<h1>Scala to Javascript compiler test page </h1>
+
+<h2>Scala Source:</h2>
+<div class="ScalaCode">""";
+    val p2 : String = """
+</div>
+
+<h2>Javascript Source:</h2>
+<div class="JSCode">""";
+    val p3 : String = """
+</div>
+
+<h2>Javascript Execution:</h2>
+<div class="JSExec"><script language="JavaScript">""";
+    val p4 : String = """</script></div>
+
+</body>""";
+    
+    return p1 + scalaSource + p2 + jsSource + p3 + jsSource + p4;
   }
 }
