67f53090feaaff1300b100b877389b52fdb9333d
Trevor Griswold <tgriswol@willamette.edu>
2012-06-25 18:17:41 -0400

    Folded the functionality from SimpleParser into Parser.

--- CompilerV0/src/compilerV0/Parser.scala
+++ CompilerV0/src/compilerV0/Parser.scala
@@ -1,6 +1,11 @@
 package compilerV0
 
-object Parser extends SimpleParser[Expr] with JavaComments with CommonLiterals {
+import scala.util.parsing.combinator._
+import java.io.Reader
+
+object Parser extends RegexParsers with PackratParsers {
+	type P[T] = PackratParser[T]
+	
 	def top = expr
   
 	lazy val expr: P[Expr] =
@@ -386,4 +391,70 @@ object Parser extends SimpleParser[Expr] with JavaComments with CommonLiterals {
         	case Nil => base
         	case (opPair : OpPair) :: tail => buildRight(BinOpExpr(opPair.getOp(), opPair.getExpr(), base), tail)
     	}
+	
+	def apply(source: String): Expr = parseAll(top, source) match {
+	    case Success(result, _) => result
+	    case ns: NoSuccess => throw new Exception(ns.msg)
+	  }
+
+	def apply(source: Reader): Expr = parseAll(top, source) match {
+	    case Success(result, _) => result
+	    case ns: NoSuccess => throw new Exception(ns.msg)
+	  }
+	
+	def ident: Parser[String] =
+    """[a-zA-Z_]\w*""".r
+    
+  def wholeNumber: Parser[String] =
+    """\d+""".r
+    
+  def floatingPointNumber: Parser[String] =
+    """(\d+\.(\d*)?|\d*\.\d+)([eE][+-]?\d+)?""".r
+  
+  /**
+   * A string literal may directly contain printable characters except " or \:
+   * - to get a \, use \\
+   * - to get a ", use \"
+   * - to get backspace, use \b
+   * - to get formfeed, use \f
+   * - to get newline, use \n
+   * - to get carriage return, use \r
+   * - to get tab, use \t
+   */
+  def stringLiteral: Parser[String] =
+    ("\"" + """([^"\p{Cntrl}\\]|\\[\\"bfnrt])*""" + "\"").r
+    
+  /**
+   * Remove surrounding quotes, and replace escaped characters in a string literal.
+   * Does no error checking.
+   * TODO support octal and hex character codes
+   * 
+   * @param s
+   */
+  def unquote(s: String): String = {
+    val buf = new StringBuilder
+    var i = 1
+    while (i < s.length - 1) {
+      s.charAt(i) match {
+        case '\\' => s.charAt(i+1) match {
+          case '\\' => buf.append('\\'); i += 1
+          case '"' => buf.append('"'); i += 1
+          case 'b' => buf.append('\b'); i += 1
+          case 'f' => buf.append('\f'); i += 1
+          case 'n' => buf.append('\n'); i += 1
+          case 'r' => buf.append('\r'); i += 1
+          case 't' => buf.append('\t'); i += 1
+          case c => buf.append(c); i += 1
+        }
+        
+        case c => buf.append(c)
+      }
+      i += 1
+    }
+    buf.toString
+  }
+}
+
+trait JavaComments { this: RegexParsers =>
+  override val whiteSpace = """(\s|(//.*\n)|(/\*(?s:.)*?\*/))*""".r
 }
