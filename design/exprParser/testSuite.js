d99054aa6d594b9aa0b7a92bd4c4a5a5f799eb83
Brian Howard <bhoward@depauw.edu>
2012-07-07 22:53:26 -0400

    Added some parser tests to the test suite

--- design/exprParser/testSuite.js
+++ design/exprParser/testSuite.js
@@ -17,6 +17,36 @@ var testSuite = {
 	
 	testFail: function() {
 		ju.fail();
+	},
+	
+	testKeywordParser: function() {
+	    var r = new RegexParsers();
+	    var p = r.keyword("test");
+	    ju.assertIdentical("Success(test, ing)", p.app("testing").toString());
+	},
+	
+	testRegexParser: function() {
+	    var r = new RegexParsers();
+	    var p = r.regex(/[1-9]\d*|0/);
+	    ju.assertIdentical("Success(23, skidoo)", p.app("23skidoo").toString());
+	},
+	
+	testRegexParser2: function() {
+	    var r = new RegexParsers();
+	    var p = r.regex(/[1-9]\d*|0/);
+	    ju.assertIdentical("Success(0, 123skidoo)", p.app("0123skidoo").toString());
+	},
+	
+	testRegexParser3: function() {
+	    var r = new RegexParsers();
+	    var p = r.regex(/[1-9]\d*|0/);
+	    ju.assertIdentical("Failure(Expected '/[1-9]\\d*|0/' got 'testing')", p.app("testing").toString());
+	},
+	
+	testSeqParser: function() {
+	    var r = new RegexParsers();
+	    var p = r.keyword("test")["~"](function() {return r.regex(/[1-9]\d*|0/);});
+	    ju.assertIdentical("Success(test~123, )", p.app("test123").toString());
 	}
 }
 
