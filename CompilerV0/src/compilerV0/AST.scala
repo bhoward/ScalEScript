1a820a810586259ce2212c85ba8efdfc79a0edfa
Trevor Griswold <tgriswol@willamette.edu>
2012-07-09 20:30:20 -0400

    Added the classes for a Typed AST

--- CompilerV0/src/compilerV0/AST.scala
+++ CompilerV0/src/compilerV0/AST.scala
@@ -1,11 +1,8 @@
 package compilerV0
 
-sealed trait Numeric
-case class NInt(num : Int) extends Numeric
-case class NDouble(Num : Double) extends Numeric
-
+//Non-Typed AST
 sealed trait Stmt {
-  def isExpr() : Boolean = {false}
+	def isExpr() : Boolean = {false}
 }
 case class VarDefStmt(ids : List[String], varType: String, value : Expr) extends Stmt
 case class ValDefStmt(ids : List[String], varType: String, value : Expr) extends Stmt
@@ -13,7 +10,7 @@ case class FunDefStmt(name : String, params : List[VarDclStmt], retType : String
 case class VarDclStmt(ids : List[String], varType: String) extends Stmt
 
 sealed trait Expr extends Stmt {
-  override def isExpr() : Boolean = {true}
+	override def isExpr() : Boolean = {true}
 }
 case class BoolExpr(bool : Boolean) extends Expr
 case class NumExpr(num: Numeric) extends Expr
@@ -26,6 +23,36 @@ case class UnOpExpr(op: String, expr: Expr) extends Expr
 case class IfThenExpr(test: Expr, trueClause: Expr) extends Expr
 case class IfThenElseExpr(test: Expr, trueClause: Expr, falseClause: Expr) extends Expr
 case class WhileExpr(test: Expr, body: Expr) extends Expr
+
+//Typed AST
+sealed trait TypedStmt {
+	def isExpr() : Boolean = {false}
+	def evalType() : Type;
+}
+case class TypedVarDefStmt(ids : List[String], varType: String, value : Expr, evalType : Type) extends TypedStmt
+case class TypedValDefStmt(ids : List[String], varType: String, value : Expr, evalType : Type) extends TypedStmt
+case class TypedFunDefStmt(name : String, params : List[VarDclStmt], retType : String, body : Expr, evalType : Type) extends TypedStmt
+case class TypedVarDclStmt(ids : List[String], varType: String, evalType : Type) extends TypedStmt
+
+sealed trait TypedExpr extends TypedStmt {
+	override def isExpr() : Boolean = {true}
+}
+case class TypedBoolExpr(bool : Boolean, evalType : Type) extends TypedExpr
+case class TypedNumExpr(num: Numeric, evalType : Type) extends TypedExpr
+case class TypedStringExpr(str : String, evalType : Type) extends TypedExpr
+case class TypedVarExpr(id: String, evalType : Type) extends TypedExpr
+case class TypedFunExpr(id: String, args : List[Expr], evalType : Type) extends TypedExpr
+case class TypedBlockExpr(body: List[Stmt], evalType : Type) extends TypedExpr
+case class TypedBinOpExpr(op: String, left: Expr, right: Expr, evalType : Type) extends TypedExpr
+case class TypedUnOpExpr(op: String, expr: Expr, evalType : Type) extends TypedExpr
+case class TypedIfThenExpr(test: Expr, trueClause: Expr, evalType : Type) extends TypedExpr
+case class TypedIfThenElseExpr(test: Expr, trueClause: Expr, falseClause: Expr, evalType : Type) extends TypedExpr
+case class TypedWhileExpr(test: Expr, body: Expr, evalType : Type) extends TypedExpr
+
+//Classes used for compiling
+sealed trait Numeric
+case class NInt(num : Int) extends Numeric
+case class NDouble(Num : Double) extends Numeric
 
 case class DefWrapper(ids : List[String], varType: String, value : Expr)
 case class FunWrapper(name : String, args : List[VarDclStmt], retType : String,  body : Expr)
