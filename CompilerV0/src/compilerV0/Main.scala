8e21389cd05a6574491fdbbb696b3c49b127a044
Trevor Griswold <tgriswol@willamette.edu>
2012-07-30 17:46:29 -0400

    Generated a test page for do loops

--- CompilerV0/src/compilerV0/Main.scala
+++ CompilerV0/src/compilerV0/Main.scala
@@ -27,7 +27,7 @@ object Main {
 		//println(Parser("""{var z:Int = 6; def foo():Int = {x+y+z};}"""));
 		//println(Parser("""object Foo {var z:Int = 6; def foo():Int = {z+z};}"""));
 
-		/*
+		
 	    testCompiler("Blocks", """println({{5; 4; ; ; ; 6;}; {}})""");
 	    testCompilerThrows("""println({{}; {var x : Int = 5; ; ; ;}})""", "The last line in the block is a Stmt, expected an Expr");
 	    testCompiler("simpleExpr", """println( 1 + 3 * 5 )""");
@@ -58,12 +58,13 @@ object Main {
 	        );
 	    testCompiler("while", """{var n: Int = 0; while (n < 10) {n = n + 1; print(n);}; println("Done");}""");
 	    testCompiler("while2", """{var n: Int = 0; while (n < 10) {n = n + 1; print(n + " ");}; println("Done");}""");
+	    testCompiler("do", """{var x: Int = 0; do {x = x + 1; println(x);} while (x < 10)}""")
 	    testCompiler("strings",
 	        "{println(\"\"\"Hello\n" +
 	        "This is a test:\\tone\\ttwo\\tthree\"\"\"); /* should not be tabbed */\n" +
 	        "println(\"This is a test:\\tone\\ttwo\\tthree\") /* should be tabbed */}");
 	    testCompiler("Curry", """println({def doSomeMath(x:Int)(y:Int)(z:Int) : Int = x*y+z; doSomeMath(2)(3)(4)})""");
-	    */
+	    
 	}
   
 	def testCompiler(testName : String, scalaSource : String) {
