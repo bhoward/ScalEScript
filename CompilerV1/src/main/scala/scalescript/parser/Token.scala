a61b5c7ed4db5e50fd67bfd8485ae2c063f1f596
Brian Howard <bhoward@depauw.edu>
2012-08-13 16:48:54 -0400

    Start of implementing separate lexer phase

---
+++ CompilerV1/src/main/scala/scalescript/parser/Token.scala
@@ -0,0 +1,124 @@
+package scalescript.parser
+
+sealed trait TokenType
+case object EOF extends TokenType
+case object ID extends TokenType
+case object INT extends TokenType
+case object DOUBLE extends TokenType
+case object STRING extends TokenType
+case object CHAR extends TokenType
+case object OP extends TokenType
+case object ASSIGN extends TokenType
+
+case class Token(ttype: TokenType, lexeme: String)
+
+object Token {
+  val whitespace =
+    """(\s|(//(?d:.)*\n)|(/\*(?s:.)*?\*/))*""".r
+
+  val integerLiteral =
+    """\d+""".r
+
+  val floatingPointLiteral =
+    """(\d+\.(\d*)?|\d*\.\d+)([eE][+-]?\d+)?|\d+([eE][+-]?\d+)""".r
+  
+  val stringLiteral =
+    ("\"" + """([^"\p{Cntrl}\\]|\\[\\"'bfnrt])*""" + "\"").r
+
+  val multilineStringLiteral =
+    ("\"\"\"" + """("?"?[^"])*"*""" + "\"\"\"").r
+    
+  val characterLiteral =
+    ("\'" + """([^\p{Cntrl}\\]|\\[\\"'bfnrt])""" + "\'").r
+
+  def lex(s: String): (Token, String) = {
+    if (s == "") {
+      (Token(EOF, ""), "")
+    } else {
+      // Strip leading whitespace
+      whitespace.findPrefixOf(s) match {
+        case Some(ws) => return Token.lex(s.substring(ws.length))
+        case None =>
+      }
+
+      if (s(0).isLetter) {
+        getIdOrKeyword(s)
+      } else if (s(0).isDigit || (s(0) == '.' && s(1).isDigit)) {
+        getNumLiteral(s)
+      } else if (s(0) == '"') {
+        getStringLiteral(s)
+      } else if (s(0) == ''') {
+        getCharLiteral(s)
+      } else {
+        getOp(s)
+      }
+    }
+  }
+
+  def getIdOrKeyword(s: String): (Token, String) = null
+
+  // pre: s starts with a digit
+  def getNumLiteral(s: String): (Token, String) = {
+    floatingPointLiteral.findPrefixOf(s) match {
+      case Some(lexeme) => (Token(DOUBLE, lexeme), s.substring(lexeme.length))
+      case None =>
+        integerLiteral.findPrefixOf(s) match {
+          case Some(lexeme) => (Token(INT, lexeme), s.substring(lexeme.length))
+          case None => throw new Exception("Failed to match numeric literal") // should not happen
+        }
+    }
+  }
+
+  // pre: s starts with "
+  def getStringLiteral(s: String): (Token, String) = {
+    if (s.startsWith("\"\"\"")) {
+      multilineStringLiteral.findPrefixOf(s) match {
+        case Some(literal) => (Token(STRING, deTriquotify(literal)), s.substring(literal.length))
+        case None => throw new Exception("Invalid multiline string literal")
+      }
+    } else {
+      stringLiteral.findPrefixOf(s) match {
+        case Some(literal) => (Token(STRING, deQuotify(literal)), s.substring(literal.length))
+        case None => throw new Exception("Invalid string literal")
+      }
+    }
+  }
+
+  // pre: s starts with '
+  def getCharLiteral(s: String): (Token, String) = {
+    characterLiteral.findPrefixOf(s) match {
+      case Some(literal) => (Token(CHAR, deQuotify(literal)), s.substring(literal.length))
+      case None => throw new Exception("Invalid character literal")
+    }
+    
+    // TODO also recognize symbols here
+  }
+
+  def getOp(s: String): (Token, String) = null
+
+  // Expects first and last characters to be either " or '
+  def deQuotify(s: String): String = {
+    val buf = new StringBuilder
+    var i = 1 // Skip first character
+    while (i < s.length - 1) {
+      s.charAt(i) match {
+        case '\\' => s.charAt(i + 1) match {
+          case 'b' => buf.append('\b'); i += 1
+          case 'f' => buf.append('\f'); i += 1
+          case 'n' => buf.append('\n'); i += 1
+          case 'r' => buf.append('\r'); i += 1
+          case 't' => buf.append('\t'); i += 1
+          case c => buf.append(c); i += 1 // Includes \, ", and ', plus any other
+        }
+        case c => buf.append(c)
+      }
+      i += 1
+    }
+    buf.toString
+  }
+
+  // Expects first and last _three_ characters to be "
+  def deTriquotify(s: String): String = {
+    s.substring(3, s.length - 3)
+  }
+}
---
+++ CompilerV1/src/main/scala/scalescript/parser/TokenParsers.scala
@@ -0,0 +1,109 @@
+package scalescript.parser
+
+/**
+ * Re-implementation of string-based combinator parser to have a separate lexing step.
+ * 
+ * @author Brian Howard
+ */
+trait TokenParsers {
+  sealed trait Result[+A]
+  case class Success[+A](value: A, rem: TokenStream) extends Result[A]
+  case class Failure(msg: String) extends Result[Nothing]
+  
+  case class ~[+A, +B](a: A, b: B)
+  
+  trait Parser[+A] extends (TokenStream => Result[A]) { outer =>
+    def ~[B](that: => Parser[B]): Parser[A ~ B] = new SequenceParser(this, that)
+    
+    def |[B >: A](that: => Parser[B]): Parser[B] = new DisParser(this, that)
+    
+    def ~>[B](that: => Parser[B]): Parser[B] =
+      this ~ that ^^ { case a ~ b => b }
+    
+    def <~[B](that: => Parser[B]): Parser[A] =
+      this ~ that ^^ { case a ~ b => a }
+    
+    def * : Parser[List[A]] =
+      ( this ~ * ^^ { case a ~ b => a :: b }
+      | success(Nil)
+      )
+      
+    def ? : Parser[Option[A]] =
+      ( this ^^ { case a => Some(a) }
+      | success(None)
+      )
+    
+    def ^^[B](f: A => B): Parser[B] = new Parser[B] {
+      def apply(ts: TokenStream): Result[B] = outer(ts) match {
+        case Success(v, rem) => Success(f(v), rem)
+        case f: Failure => f
+      }
+    }
+  }
+  
+  implicit def token(tt: TokenType): Parser[Token] = new Parser[Token] {
+    def apply(ts: TokenStream): Result[Token] = {
+      if (ts.head.ttype == tt) {
+        Success(ts.head, ts.tail)
+      } else {
+        Failure("Expected '%s' got '%s'".format(tt, ts.head.lexeme))
+      }
+    }
+  }
+
+  class SequenceParser[+A, +B](l: => Parser[A], r: => Parser[B]) extends Parser[A ~ B] {
+    lazy val left = l
+    lazy val right = r
+    
+    def apply(ts: TokenStream): Result[A ~ B] = left(ts) match {
+      case Success(a, rem) => right(rem) match {
+        case Success(b, rem2) => Success(new ~(a, b), rem2)
+        case f: Failure => f
+      }
+      case f: Failure => f
+    }
+  }
+  
+  class DisParser[+A](l: => Parser[A], r: => Parser[A]) extends Parser[A] {
+    lazy val left = l
+    lazy val right = r
+    
+    def apply(ts: TokenStream): Result[A] = {
+      val res = left(ts)
+      res match {
+        case Success(_, _) => res
+        case _ => right(ts)
+      }
+    }
+  }
+  
+  def success[A](v: A) = new Parser[A] {
+    def apply(ts: TokenStream): Result[A] = Success(v, ts)
+  }
+  
+  def failure(msg: String) = new Parser[Nothing] {
+    def apply(ts: TokenStream): Result[Nothing] = Failure(msg)
+  }
+  
+  def phrase[A](p: Parser[A]) = new Parser[A] {
+    def apply(ts: TokenStream): Result[A] = p.apply(ts) match {
+      case Success(a, rem) =>
+        if (rem.isEmpty) {
+          Success(a, rem)
+        } else {
+          Failure("Expected end of input, got '%s...'".format(rem.head))
+        }
+      case f: Failure => f
+    }
+  }
+
+  def parseAll[A](p: Parser[A], ts: TokenStream): Result[A] =
+    phrase(p).apply(ts)
+}
+
+// TODO implement this
+trait TokenStream {
+  def head: Token
+  def tail: TokenStream
+  def isEmpty: Boolean
+}
